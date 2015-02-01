70b0e33e5ee0fd9923644a52aaeb3507f894ed4a
Thames1990 <ThomasMohr1990@googlemail.com>
2015-02-01 09:36:09 +0100

    Updated scripts Added optimized preview images Minor code cleanup (Still a lot to work on)

--- js/init.js
+++ js/init.js
@@ -11,8 +11,17 @@
 
         reset: 'full',
         breakpoints: {
-            global: {href: 'css/style.css', containers: '45em', grid: {gutters: ['2em', 0]}},
-            xlarge: {media: '(max-width: 1680px)', href: 'css/style-xlarge.css'},
+            global: {
+                href: 'css/style.css',
+                containers: '45em',
+                grid: {
+                    gutters: ['2em', 0]
+                }
+            },
+            xlarge: {
+                media: '(max-width: 1680px)',
+                href: 'css/style-xlarge.css'
+            },
             large: {
                 media: '(max-width: 1280px)',
                 href: 'css/style-large.css',
@@ -20,14 +29,27 @@
                 grid: {gutters: ['1.5em', 0]},
                 viewport: {scalable: false}
             },
-            medium: {media: '(max-width: 1024px)', href: 'css/style-medium.css', containers: '85%!', grid: {zoom: 2}},
+            medium: {
+                media: '(max-width: 1024px)',
+                href: 'css/style-medium.css',
+                containers: '85%!',
+                grid: {
+                    zoom: 2
+                }
+            },
             small: {
                 media: '(max-width: 736px)',
                 href: 'css/style-small.css',
                 containers: '90%!',
                 grid: {gutters: ['1.25em', 0]}
             },
-            xsmall: {media: '(max-width: 480px)', href: 'css/style-xsmall.css', grid: {zoom: 3}}
+            xsmall: {
+                media: '(max-width: 480px)',
+                href: 'css/style-xsmall.css',
+                grid: {
+                    zoom: 3
+                }
+            }
         },
         plugins: {
             layers: {
