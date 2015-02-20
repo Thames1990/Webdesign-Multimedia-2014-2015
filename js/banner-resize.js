778c49ee7097a338de0dd55674d7841070efc450
Shuugo <chaos@shuugo.de>
2015-02-20 20:58:05 +0100

    Banner js ausgelagert

---
+++ js/banner-resize.js
@@ -0,0 +1,10 @@
+function banner() {
+    var header_height = $("header").outerHeight();
+
+    $('head').append('<style>' +
+    '#one:before{' +
+    'height: ' + header_height + 'px;' +
+    '}' +
+    '</style>')
+
+}
